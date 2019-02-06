994963412763cb2a6c2c5709fe34ce08e0cda76d
terence <terence@coffeeshopstudio.ca>
2019-02-06 13:05:24 -0700

    implemented basics of image picker

--- app/src/main/java/ca/coffeeshopstudio/gaminginterfaceclient/models/ImageAdapter.java
+++ app/src/main/java/ca/coffeeshopstudio/gaminginterfaceclient/models/ImageAdapter.java
@@ -6,9 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
+import android.widget.TextView;
 
 import ca.coffeeshopstudio.gaminginterfaceclient.R;
 
@@ -16,19 +14,18 @@ import ca.coffeeshopstudio.gaminginterfaceclient.R;
  * TODO: HEADER COMMENT HERE.
  */
 public class ImageAdapter extends BaseAdapter {
-    private static final String[] IMAGE_URLS = Constants.IMAGES;
-    Context c;
+    private static final int[] builtIn = Constants.buttons;
+    Context context;
     private LayoutInflater inflater;
 
     public ImageAdapter(Context context) {
         inflater = LayoutInflater.from(context);
-        c = context;
-
+        this.context = context;
     }
 
     @Override
     public int getCount() {
-        return IMAGE_URLS.length;
+        return builtIn.length + 1;
     }
 
     @Override
@@ -45,65 +42,69 @@ public class ImageAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         final ViewHolder holder;
         View view = convertView;
+        //build / retrieve a cell in the grid
         if (view == null) {
             view = inflater.inflate(R.layout.item_grid_image, parent, false);
             holder = new ViewHolder();
             assert view != null;
 
+            holder.textView = view.findViewById(R.id.text);
             holder.imageView = view.findViewById(R.id.image);
 
             view.setTag(holder);
         } else {
             holder = (ViewHolder) view.getTag();
         }
-        Picasso.get().setLoggingEnabled(true);
+
+
+        //Picasso.get().setLoggingEnabled(true);
         if (position == 0) {
-            holder.imageView.setImageResource(R.drawable.neon_button);
+            holder.textView.setVisibility(View.VISIBLE);
+            holder.imageView.setVisibility(View.GONE);
+            //show "add" item
+        } else if (position <= builtIn.length) {
+            holder.textView.setVisibility(View.GONE);
+            holder.imageView.setVisibility(View.VISIBLE);
+            holder.imageView.setImageResource(builtIn[position-1]);
             holder.imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
         } else {
-            Picasso.get()
-                    .load(IMAGE_URLS[position])
-                    .placeholder(R.mipmap.ic_launcher)
-                    .error(R.mipmap.ic_launcher)
-                    .fit()
-                    .into(holder.imageView, new Callback() {
-
-                        @Override
-                        public void onSuccess() {
-                            holder.imageView.setVisibility(View.VISIBLE);
-                        }
-
-                        @Override
-                        public void onError(Exception e) {
-                            holder.imageView.setVisibility(View.INVISIBLE);
-                        }
-                    });
+            //show user generated images
+            holder.textView.setVisibility(View.GONE);
+            holder.imageView.setVisibility(View.VISIBLE);
         }
 
         return view;
+//            Picasso.get()
+//                    .load(builtIn[position])
+//                    .placeholder(R.mipmap.ic_launcher)
+//                    .error(R.mipmap.ic_launcher)
+//                    .fit()
+//                    .into(holder.imageView, new Callback() {
+//
+//                        @Override
+//                        public void onSuccess() {
+//                            holder.imageView.setVisibility(View.VISIBLE);
+//                        }
+//
+//                        @Override
+//                        public void onError(Exception e) {
+//                            holder.imageView.setVisibility(View.INVISIBLE);
+//                        }
+//                    });
+//        }
     }
 }
 
 class ViewHolder {
     ImageView imageView;
+    TextView textView;
 }
 
 
 class Constants {
 
-    public static final String[] IMAGES = new String[]{
-// Heavy images
-            "R.drawable.neon_button",
-            "file:///sdcard/blue-button-for-web.jpg",
-            "file:///sdcard/blue-button-for-web.jpg",
-            "file:///sdcard/blue-button-for-web.jpg",
-            "file:///sdcard/blue-button-for-web.jpg",
-            "https://via.placeholder.com/150",
-            "https://via.placeholder.com/150",
-            "https://via.placeholder.com/150",
-            "https://via.placeholder.com/150",
-            "https://via.placeholder.com/150",
-            "https://via.placeholder.com/150",
+    static final int[] buttons = new int[]{
+        R.drawable.neon_button,
     };
 
 
