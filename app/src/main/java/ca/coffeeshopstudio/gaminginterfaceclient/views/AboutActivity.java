fd2006c6aa408b02009babfcbf16ec7164e478ba
terence <terence@coffeeshopstudio.ca>
2019-02-19 20:23:35 -0700

    fixed about toolbar

--- app/src/main/java/ca/coffeeshopstudio/gaminginterfaceclient/views/AboutActivity.java
+++ app/src/main/java/ca/coffeeshopstudio/gaminginterfaceclient/views/AboutActivity.java
@@ -7,6 +7,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
@@ -37,6 +38,9 @@ public class AboutActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.title_activity_about);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         if (fab != null) {
