659ffdc909a7a97a72985e4da2c7d8f5df97483f
Terence <terence@coffeeshopstudio.ca>
2019-10-18 12:09:11 -0600

    fixed test not compiling (tests not written tho)

--- gic_flutter/test/widget_test.dart
+++ gic_flutter/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:gic_flutter/main_gplay.dart';
+import 'package:gic_flutter/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(null));
+    await tester.pumpWidget(GicApp(null));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
