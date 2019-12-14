7050dcd4cc59a20dceb67c71de1feedceadda7ce
Terence Doerksen <terence@coffeeshopstudio.ca>
2019-12-13 21:33:47 -0700

    Mainrepo ut (#105)
    * added first unit test for main repo
    
    * updated travis config
    
    * changes required for new flutter version
    
    * working on tests
    
    * fixed unit test name
    
    * implemented 3 tests successfully
    
    * additional unit tests

---
+++ gic_flutter/test/views/main/mainRepo_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gic_flutter/model/channel.dart';
+import 'package:gic_flutter/views/main/mainRepo.dart';
+import 'package:gic_flutter/views/main/mainVM.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MockRepoContract implements MainRepoContract {
+  MainVM vm;
+
+  void preferencesLoaded(MainVM viewModel) {
+    vm = viewModel;
+  }
+}
+
+void main() {
+  test('on new install need to convert should be true', () {
+    //Assign
+    SharedPreferences.setMockInitialValues({}); //set values here
+    MockRepoContract contract = new MockRepoContract();
+    MainRepo repo = new MainRepo(contract);
+
+    //Act
+    await repo.fetch();
+
+    //Assert
+    expect(true, contract.vm.firstRun);
+  });
+
+//  test('on old install first run should be false', () {
+//    //Assign
+//    SharedPreferences.setMockInitialValues(<String, dynamic>{_prefixedKey: false});
+//
+//
+//    MockRepoContract contract = new MockRepoContract();
+//    MainRepo repo = new MainRepo(contract);
+//
+//    //Act
+//    repo.fetch();
+//
+//    //Assert
+//    expect(true, contract.vm.firstRun);
+//  });
+}
