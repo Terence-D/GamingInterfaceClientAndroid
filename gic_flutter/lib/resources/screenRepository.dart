8bd9bfe78dea33283cd8b66bf1935f98c00dc3d0
Terence <terence@coffeeshopstudio.ca>
2020-12-16 08:02:30 -0700

    added vscode launch json fixed starting wrong screen formatting that better matches darts opinionated views.

--- gic_flutter/lib/resources/screenRepository.dart
+++ gic_flutter/lib/resources/screenRepository.dart
@@ -21,7 +21,7 @@ class ScreenRepository {
 
   int defaultBackground = 0xFF383838;
 
-  _load (SharedPreferences prefs) async {
+  _load(SharedPreferences prefs) async {
     _cache = new List<Screen>();
     await prefs.reload();
     Set<String> keys = prefs.getKeys();
@@ -30,7 +30,8 @@ class ScreenRepository {
       if (key.contains(_prefsScreen)) {
         int screenId = int.parse(key.substring(_prefsScreen.length));
         Screen screen = new Screen(screenId: screenId);
-        try {//legacy used an integer dummy value, so need to handle that
+        try {
+          //legacy used an integer dummy value, so need to handle that
           screen.name = prefs.getString(key);
         } catch (_) {
           screen.name = "Screen $screenId";
@@ -43,9 +44,10 @@ class ScreenRepository {
   }
 
   _loadBackground(SharedPreferences prefs, Screen screen) {
-    
-    int backgroundColor = prefs.getInt("${screen.screenId}$_prefsBackgroundSuffix");
-    String backgroundPath = prefs.getString("${screen.screenId}$_prefsBackgroundPathSuffix");
+    int backgroundColor =
+        prefs.getInt("${screen.screenId}$_prefsBackgroundSuffix");
+    String backgroundPath =
+        prefs.getString("${screen.screenId}$_prefsBackgroundPathSuffix");
     screen.backgroundColor = backgroundColor;
     screen.backgroundPath = backgroundPath;
   }
@@ -54,7 +56,8 @@ class ScreenRepository {
     screen.controls = new List<GicControl>();
     prefs.getKeys().forEach((key) {
       if (key.contains("${screen.screenId}$_prefsControl")) {
-        try {//legacy used an integer dummy value, so need to handle that
+        try {
+          //legacy used an integer dummy value, so need to handle that
           Map controlMap = jsonDecode(prefs.getString(key));
           screen.controls.add(GicControl.fromJson(controlMap));
           //screen.name = prefs.getString(key);
@@ -99,8 +102,10 @@ class ScreenRepository {
 
   _save(SharedPreferences prefs, Screen screen) {
     prefs.setString("$_prefsScreen${screen.screenId}", screen.name);
-    prefs.setInt("${screen.screenId}$_prefsBackgroundSuffix", screen.backgroundColor);
-    prefs.setString("${screen.screenId}$_prefsBackgroundPathSuffix", screen.backgroundPath);
+    prefs.setInt(
+        "${screen.screenId}$_prefsBackgroundSuffix", screen.backgroundColor);
+    prefs.setString(
+        "${screen.screenId}$_prefsBackgroundPathSuffix", screen.backgroundPath);
 
     //clear out the old
     prefs.getKeys().forEach((key) {
@@ -109,7 +114,7 @@ class ScreenRepository {
     });
 
     //add the updated controls
-    int i=0;
+    int i = 0;
     if (screen.controls != null && screen.controls.length > 0) {
       screen.controls.forEach((control) {
         String json = jsonEncode(control.toJson());
@@ -119,22 +124,25 @@ class ScreenRepository {
     }
   }
 
-  updateName (int id, String newName) async {
+  updateName(int id, String newName) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString("$_prefsScreen$id", newName);
   }
 
-  loadFromJson(List<ScreenItem> screens, String device, BuildContext context) async {
+  loadFromJson(
+      List<ScreenItem> screens, String device, BuildContext context) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     if (_cache == null || _cache.length < 1)
       await _load(prefs);
 
-    //the name will have spaces, but the asset file does not.  so turn Large Tablet into LargeTablet
+    //the name will have spaces, but the asset file does not.
+    // so turn Large Tablet into LargeTablet
     device = device.replaceAll(" ", ""); //remove spaces
 
     //load in the asset and decode the json
-    for (int n=0; n < screens.length; n++) {
-      String jsonString = await DefaultAssetBundle.of(context).loadString("assets/screens/${screens[n].title}-$device.json");
+    for (int n = 0; n < screens.length; n++) {
+      String jsonString = await DefaultAssetBundle.of(context)
+          .loadString("assets/screens/${screens[n].title}-$device.json");
       Map controlMap = jsonDecode(jsonString);
 
       //build the new screen from the incoming json
@@ -173,10 +181,10 @@ class ScreenRepository {
   Future save(Screen newScreen) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     _save(prefs, newScreen);
-     if (_cache == null) {
+    if (_cache == null) {
       _cache = new List<Screen>();
     }
-   _cache.add(newScreen);
+    _cache.add(newScreen);
   }
 
   Future<int> delete(int id) async {
@@ -195,15 +203,16 @@ class ScreenRepository {
         prefs.remove(key);
     });
 
-    for (int i=0; i < _cache.length; i++) {
+    for (int i = 0; i < _cache.length; i++) {
       if (_cache[i].screenId == id) {
-        if (_cache[i].backgroundPath != null && _cache[i].backgroundPath.isNotEmpty) {
-          final File background = new File (_cache[i].backgroundPath);
+        if (_cache[i].backgroundPath != null &&
+            _cache[i].backgroundPath.isNotEmpty) {
+          final File background = new File(_cache[i].backgroundPath);
           background.delete();
         }
         _cache[i].controls.forEach((element) {
           if (element.primaryImage.contains("_control_")) {
-            final File control = new File (element.primaryImage);
+            final File control = new File(element.primaryImage);
             control.delete();
           }
         });
@@ -272,10 +281,14 @@ class ScreenRepository {
         if (path.extension(element.path).toLowerCase() == ".png") {
           //check if it's a background image or control, if so it's a simple
           //change, just change the start of the filename to the new screen id
-          if (path.basenameWithoutExtension(element.path).contains("_background")) {
+          if (path
+              .basenameWithoutExtension(element.path)
+              .contains("_background")) {
             _renameBackground(element, screen, files);
           }
-          if (path.basenameWithoutExtension(element.path).contains("_control_")) {
+          if (path
+              .basenameWithoutExtension(element.path)
+              .contains("_control_")) {
             _renameImage(element, screen, files);
           }
           // check if it's a button or switch, if so:
@@ -291,20 +304,21 @@ class ScreenRepository {
           } else if (element.path.contains("switch_")) {
             _renameControl(element, foundSwitchIds, screen, files, "switch_");
           }
-
         }
       }
     });
   }
 
-  void _renameControl(File element, Map<int, int> foundIds, Screen screen, Directory files, String searchParam) {
+  void _renameControl(File element, Map<int, int> foundIds, Screen screen,
+      Directory files, String searchParam) {
     String fileName = path.basenameWithoutExtension(element.path);
     int separatorPosition = fileName.indexOf("_");
-    int oldId = int.tryParse(fileName.substring(separatorPosition+1));
+    int oldId = int.tryParse(fileName.substring(separatorPosition + 1));
     if (oldId != null) {
       //check if we've seen this before
       if (foundIds.containsKey(oldId)) {
-        String newFilename = "${fileName.substring(0, separatorPosition+1)}_${foundIds[oldId]}";
+        String newFilename =
+            "${fileName.substring(0, separatorPosition + 1)}_${foundIds[oldId]}";
         //found before, so just update references
         screen.controls.forEach((control) {
           if (control.primaryImage.contains("${searchParam}_$oldId")) {
@@ -318,11 +332,12 @@ class ScreenRepository {
         //we haven't seen this id before
         int originalId = oldId; //back it up for later
         //we'll look through and search until we find a no match
-        String newFilename = "${fileName.substring(0, separatorPosition+1)}$oldId";
+        String newFilename =
+            "${fileName.substring(0, separatorPosition + 1)}$oldId";
         String searchFor = path.join(files.path, "$newFilename.png");
         List<FileSystemEntity> dirList = files.listSync();
-        for (int i=0; i <dirList.length; i++) {
-          newFilename = "${fileName.substring(0, separatorPosition+1)}$oldId";
+        for (int i = 0; i < dirList.length; i++) {
+          newFilename = "${fileName.substring(0, separatorPosition + 1)}$oldId";
           searchFor = path.join(files.path, "$newFilename.png");
           if (dirList[i].path == searchFor) {
             oldId++;
@@ -330,17 +345,17 @@ class ScreenRepository {
           }
         }
 
-        newFilename = "${fileName.substring(0, separatorPosition+1)}$oldId";
+        newFilename = "${fileName.substring(0, separatorPosition + 1)}$oldId";
 
         //found a new id, update file and references
-        newFilename =  path.join(files.path, "$newFilename.png");
+        newFilename = path.join(files.path, "$newFilename.png");
 
         screen.controls.forEach((control) {
           if (control.primaryImage.contains("${searchParam}_$oldId")) {
-            control.primaryImage =  newFilename;
+            control.primaryImage = newFilename;
           }
           if (control.secondaryImage.contains("${searchParam}_$oldId")) {
-            control.secondaryImage =  newFilename;
+            control.secondaryImage = newFilename;
           }
         });
         element.copy(newFilename);
@@ -350,10 +365,8 @@ class ScreenRepository {
   }
 
   void _renameImage(File element, Screen screen, Directory files) {
-    String newFilename = path.join(files.path, "${_findElementToRename(element, files)}.png");
-//    String fileName = path.basenameWithoutExtension(element.path);
-//    int separatorPosition = fileName.indexOf("_");
-//    int oldId = int.tryParse(fileName.substring(0, separatorPosition));
+    String newFilename =
+        path.join(files.path, "${_findElementToRename(element, files)}.png");
     screen.controls.forEach((control) {
       if (control.primaryImage.contains(path.basename(element.path))) {
         control.primaryImage = newFilename;
@@ -380,7 +393,7 @@ class ScreenRepository {
       //we'll look through and search until we find a no match
       String searchParam = path.join(files.path, "$newFilename.png");
       List<FileSystemEntity> dirList = files.listSync();
-      for (int i=0; i <dirList.length; i++) {
+      for (int i = 0; i < dirList.length; i++) {
         newFilename = "$oldId${fileName.substring(separatorPosition)}";
         searchParam = path.join(files.path, "$newFilename.png");
         if (dirList[i].path == searchParam) {
@@ -411,8 +424,8 @@ class ScreenRepository {
       if (file.isFile) {
         final data = file.content as List<int>;
         File(path.join(tempPath, filename))
-        ..createSync(recursive: true)
-        ..writeAsBytesSync(data);
+          ..createSync(recursive: true)
+          ..writeAsBytesSync(data);
       }
     }
   }
@@ -447,38 +460,33 @@ class ScreenRepository {
   Future<int> _exportScreen(screen, cache, exportPath, filesPath) async {
     String rawJson = jsonEncode(screen);
     //store the json data file in the directory
-    File jsonData =new File(path.join(cache.path, "data.json"));
+    File jsonData = new File(path.join(cache.path, "data.json"));
     await jsonData.writeAsString(rawJson);
 
     var archive = ZipFileEncoder();
     archive.create(path.join(exportPath, screen.name + ".zip"));
     archive.addFile(jsonData);
     if (screen.backgroundPath != null && screen.backgroundPath.isNotEmpty) {
-      File background = new File (path.join(
+      File background = new File(path.join(
         filesPath,
         path.split(screen.backgroundPath).last,
-      )
-      );
+      ));
       archive.addFile(background);
     }
     screen.controls.forEach((control) {
       if (control.primaryImage != null && control.primaryImage.isNotEmpty)
-        archive.addFile(new File (path.join(
+        archive.addFile(new File(path.join(
           filesPath,
           path.split(control.primaryImage).last,
-        )
-        ));
+        )));
       if (control.secondaryImage != null && control.secondaryImage.isNotEmpty)
-        archive.addFile(new File (path.join(
+        archive.addFile(new File(path.join(
           filesPath,
           path.split(control.secondaryImage).last,
-        )
-        ));
+        )));
     });
 
     archive.close();
     return 0;
   }
-
-
 }
