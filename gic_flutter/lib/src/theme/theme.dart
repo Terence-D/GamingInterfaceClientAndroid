75cc879c6581834a095b31aa74c714a6d6a34094
Terence <terence@coffeeshopstudio.ca>
2021-04-08 15:54:36 -0600

    switched from depreciated inherit code to non depreciated variant

--- gic_flutter/lib/src/theme/theme.dart
+++ gic_flutter/lib/src/theme/theme.dart
@@ -45,14 +45,12 @@ class CustomTheme extends StatefulWidget {
   CustomThemeState createState() => new CustomThemeState();
 
   static ThemeData of(BuildContext context) {
-    _CustomTheme inherited =
-        (context.inheritFromWidgetOfExactType(_CustomTheme) as _CustomTheme);
+    _CustomTheme inherited = context.dependOnInheritedWidgetOfExactType<_CustomTheme>();
     return inherited.data.theme;
   }
 
   static CustomThemeState instanceOf(BuildContext context) {
-    _CustomTheme inherited =
-        (context.inheritFromWidgetOfExactType(_CustomTheme) as _CustomTheme);
+    _CustomTheme inherited = context.dependOnInheritedWidgetOfExactType<_CustomTheme>();
     return inherited.data;
   }
 }
