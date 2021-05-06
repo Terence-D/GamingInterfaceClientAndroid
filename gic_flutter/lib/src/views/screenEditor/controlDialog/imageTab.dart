5bd166bff73ee7a49471ec951216c99e9f1d7f88
Terence Doerksen <terence@coffeeshopstudio.ca>
2021-05-05 22:07:22 -0600

    added color picker to image tab

--- gic_flutter/lib/src/views/screenEditor/controlDialog/imageTab.dart
+++ gic_flutter/lib/src/views/screenEditor/controlDialog/imageTab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gic_flutter/src/backend/models/intl/intlScreenEditor.dart';
 import 'package:gic_flutter/src/backend/models/screen/viewModels/controlViewModel.dart';
+import 'package:gic_flutter/src/views/screenEditor/colorPickerDialog.dart';
 import 'package:gic_flutter/src/views/screenEditor/gicEditControl.dart';
 
 enum dimensions { left, top, width, height }
@@ -25,7 +26,7 @@ class ImageTabState extends State<ImageTab> {
         children: [
           _imageToggle(),
           _imageButton(),
-          _colorButton(),
+          _colorButton(0),
           Visibility(
             visible: widget.gicEditControl.control.type ==
                     ControlViewModelType.Button ||
@@ -36,7 +37,7 @@ class ImageTabState extends State<ImageTab> {
             child: Column(
               children: [
                 _imageButton(),
-                _colorButton(),
+                _colorButton(1),
               ],
             ),
           ),
@@ -84,16 +85,34 @@ class ImageTabState extends State<ImageTab> {
     return Visibility(
         visible:
             widget.gicEditControl.control.design == ControlDesignType.Image,
-        child: ElevatedButton(onPressed: () {
-          setState(() {
-          });
-        }, child: Text("Image")));
+        child: ElevatedButton(
+            onPressed: () {
+              setState(() {});
+            },
+            child: Text("Image")));
   }
 
-  Widget _colorButton() {
+  Widget _colorButton(int index) {
     return Visibility(
         visible: widget.gicEditControl.control.design ==
             ControlDesignType.UpDownGradient,
-        child: ElevatedButton(onPressed: () {}, child: Text("Color")));
+        child: ElevatedButton(
+            onPressed: () {
+              _pickBackgroundColor(index);
+            },
+            child: Text("Color")));
+  }
+
+  void _pickBackgroundColor(int index) {
+    showDialog(
+        context: context,
+        builder: (_) => ColorPickerDialog(
+            title: "test",
+            pickerColor: widget.gicEditControl.control.colors[index],
+            onPressedCallback: (Color color) {
+              setState(() {
+                widget.gicEditControl.control.colors[index] = color;
+              });
+            }));
   }
 }
