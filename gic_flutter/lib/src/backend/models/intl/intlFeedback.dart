b77de09ba25088dd8842beae23fadfe511b60f83
Terence Doerksen <terence@coffeeshopstudio.ca>
2021-10-15 22:00:35 -0600

    fixed #246

--- gic_flutter/lib/src/backend/models/intl/intlFeedback.dart
+++ gic_flutter/lib/src/backend/models/intl/intlFeedback.dart
@@ -14,11 +14,10 @@ class IntlFeedback {
     'en': {
       FeedbackText.toolbarTitle: 'Feedback',
       FeedbackText.details:
-          'Got an idea for a feature you want?  See something you hate?  Feedback is always appreciated!  You can open an issue directly on GitHub (3rd party website, link below), or if you prefer send me an email.  Although I will not share any information sent, please ensure you do not include any sensitive information, this is just a hobby not a business!',
-
+          'Got an idea for a feature you want?  See something you hate?  Feedback is always appreciated!  You can open an issue directly on GitHub (3rd party website, link below), or if you prefer send me an email.  Although I will not share any information sent, please ensure you do not include any sensitive information!',
       FeedbackText.githubIssues: 'Github Issue Tracker',
-      FeedbackText.githubIssuesUrl: 'https://github.com/Terence-D/GamingInterfaceClientAndroid/issues',
-
+      FeedbackText.githubIssuesUrl:
+          'https://github.com/Terence-D/GamingInterfaceClientAndroid/issues',
       FeedbackText.satisfaction: 'Satisfaction',
       FeedbackText.updown:
           'Tap on one of the images below (optionally) to quickly provide a thumbs up or down!',
